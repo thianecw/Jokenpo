f269da4fab5338f00697a4490e2117d382e56aab
thianecw <thianecw@hotmail.com>
2024-01-11 21:09:13 +0000

    new logo+enum

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ const machineScore = document.querySelector("#machine-score")
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+const GAME_OPTIONS = {
+ROCK: "rock",
+PAPER: "paper",
+SCISSORS: "scissors"
+}
+
 
 const playHuman = (humanChoice) => {
     playGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
@@ -26,9 +32,9 @@ const playGame = (human, machine) => {
 
         //VITÓRIA//
     } else if (
-        (human === 'paper' && machine === 'rock') ||
-        (human === 'rock' && machine === 'scissors') ||
-        (human === 'scissors' && machine === 'paper')
+        (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+        (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+        (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
     ) {
         humanScoreNumber++
         humanScore.innerHTML= humanScoreNumber
